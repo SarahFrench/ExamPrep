131a92e3caaa1089aebbef5ec59ca6d9dc995d5c
Sarah French <sarah@sarahfrench.co.uk>
2020-01-26 16:43:41 +0000

    Again, to overcome CORS issues, using XHR to request an image hosted on a local Node.js server

--- chapter-3/obj-3-4-serialize-deserialize-transmit-data/static-serve.js
+++ chapter-3/obj-3-4-serialize-deserialize-transmit-data/static-serve.js
@@ -5,6 +5,6 @@
 
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8083, function(){
-    console.log('Server running on 8083...');
+connect().use(serveStatic(__dirname)).listen(8082, function(){
+    console.log('Server running on 8082...');
 });
